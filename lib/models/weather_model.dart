516a490fbb359da3075fa7426c67932df4624e89
tolunaykutlu <tolunaykt28@gmail.com>
2023-07-19 13:41:03 +0300

    first commit

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherController extends GetxController {
+  var isLoading = true.obs;
+  var temperature = ''.obs;
+  var description = ''.obs;
+  var location = ''.obs;
+  var country = ''.obs;
+  var cityName = "istanbul".obs;
+
+  Future<void> fetchWeather() async {
+    var apiKey = '8fe320e393bc67139f0c3068cb3ce872';
+
+    var url =
+        'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey';
+    try {
+      var response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        var result = json.decode(response.body);
+        temperature.value =
+            (result['main']['temp'] - 273.15).toStringAsFixed(1);
+        description.value = result['weather'][0]['description'];
+        location.value = result['name'];
+        country.value = result["sys"]["country"];
+      }
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
---
+++ lib/views/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../models/weather_model.dart';
+
+class WeatherPage extends StatelessWidget {
+  final weatherController = Get.put(WeatherController());
+
+  WeatherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController cityController = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Weather')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: cityController,
+            ),
+            Obx(() => weatherController.isLoading.value
+                ? const CircularProgressIndicator()
+                : Text(
+                    '${weatherController.temperature.value}°C\n${weatherController.description.value}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 24),
+                  )),
+            const SizedBox(height: 16),
+            Obx(() => Text(
+                  weatherController.location.value,
+                  style: const TextStyle(fontSize: 20),
+                )),
+            Obx(() => Text(
+                  weatherController.country.value,
+                  style: const TextStyle(fontSize: 20),
+                )),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                weatherController.cityName = cityController.text.obs;
+                weatherController.fetchWeather();
+              },
+              child: const Text('Refresh'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
